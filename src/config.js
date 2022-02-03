d6aa75c2346588ab3ef1dd90947d6d983e61aab4
root <root@nolacoder.localdomain>
2022-02-03 07:35:07 -0600

    Merged from main branch

--- src/config.js
+++ src/config.js
@@ -1,84 +1,88 @@
-const path = require('path')
-const miniCss = require('mini-css-extract-plugin')
-const { env } = require('process')
+const path = require("path");
+const miniCss = require("mini-css-extract-plugin");
+const { env } = require("process");
 
 
-const styles = (env,argv) => {
-    
-    if(env.environment == "development"){
-        console.log("------------------------------------------------")
-        console.log("---------- Development Styles Build -----------")
-        console.log("------------------------------------------------")
-        console.log(" ")
-    }
-    else {
-        console.log("------------------------------------------------")
-        console.log("---------- Production Styles Build ------------")
-        console.log("------------------------------------------------")
-    }
+console.log(env)
 
 
-    return {
-    entry: './src/styles.js',
-    module:{
-        rules:[
+const styles = (env, argv) => {
+  if (env.environment == "development") {
+    console.log("------------------------------------------------");
+    console.log("---------- Development Styles Build -----------");
+    console.log("------------------------------------------------");
+    console.log(" ");
+  } else {
+    console.log("------------------------------------------------");
+    console.log("---------- Production Styles Build ------------");
+    console.log("------------------------------------------------");
+  }
+
+  return {
+    entry: "./src/styles.js",
+    module: {
+      rules: [
+        {
+          test: /\.(sass|scss)$/,
+          use: [
+            miniCss.loader,
             {
-                test: /\.(sass|scss)$/,
-                use: [miniCss.loader,{
-                    loader: 'css-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                },{
-                    loader: 'sass-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                }]
-            }
-        ]
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
     },
-    plugins:[
-        new miniCss({
-            filename: '../ctmd/css/site.css'
-        })
+    plugins: [
+      new miniCss({
+        filename: "../ctmd/css/site.css",
+      }),
     ],
-    mode: env.environment == 'development' ? 'development' : 'production'
-}
-}
+    mode: env.environment == "development" ? "development" : "production",
+  };
+};
 
-const scripts = (env,argv)=>{
+const scripts = (env, argv) => {
+  if (env.environment == "development") {
+    console.log("------------------------------------------------");
+    console.log("---------- Development Scripts Build -----------");
+    console.log("------------------------------------------------");
+    console.log(" ");
+  } else {
+    console.log("------------------------------------------------");
+    console.log("---------- Production Scripts Build ------------");
+    console.log("------------------------------------------------");
+  }
 
-    if(env.environment == "development"){
-        console.log("------------------------------------------------")
-        console.log("---------- Development Scripts Build -----------")
-        console.log("------------------------------------------------")
-        console.log(" ")
-    }
-    else {
-        console.log("------------------------------------------------")
-        console.log("---------- Production Scripts Build ------------")
-        console.log("------------------------------------------------")
-    }
-
-    return {
-    entry: './src/scripts.js',
+  return {
+    entry: "./src/scripts.js",
     output: {
-        path: path.resolve(__dirname,'../ctmd/js'),
-        filename: 'site.js'
+      path: path.resolve(__dirname, "../ctmd/js"),
+      filename: "site.js",
     },
     module: {
-        rules:[
-            {
-                test: /\.(js)$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
+      rules: [
+        {
+          test: /\.(js)$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+      ],
     },
-    mode: env.environment == 'development' ? 'development' : 'production'
-    }
-}
+    mode: env.environment == "development" ? "development" : "production",
+  };
+};
 
-module.exports.styles = styles
-module.exports.scripts = scripts
+
+module.exports.styles = styles;
+module.exports.scripts = scripts;
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
 const config = require('./src/config')
+const { env } = require('process')
+
+
 
 module.exports = [config.styles,config.scripts]
