d2454d8d57599bf86141cd12924437eeec8ff644
root <root@nolacoder.localdomain>
2022-02-28 17:17:35 -0600

    Add more categories section

--- single.php
+++ single.php
@@ -7,7 +7,14 @@
  * @package _s
  */
 
-
+   global $post;
    $context = Timber::context();
+   $context['catArticles'] = Timber::get_posts([
+     'posts_per_page' => 3,
+     'cat' => get_category($post->ID)->cat_ID
+   ]);
+
    $context['post'] = new Timber\Post();
+
+
    Timber::render('post/index.twig',$context);
